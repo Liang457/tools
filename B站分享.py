a0ed5d3cd559173784ef9376a2f650d1bba34400
Cool-GK <2338638447@qq.com>
2023-01-10 20:50:56 +0800

    23.01.10.1

---
+++ B站分享.py
@@ -0,0 +1,32 @@
+# 用以获取一个比较好的分享文本
+import urllib.request
+import json
+import os
+import logging
+
+bvid = input('输入稿件BV号(链接也可以): ')
+
+if (bvid[:31].lower() == 'https://www.bilibili.com/video/' and bvid[:31] != '') or (bvid[:15].lower() == 'https://b23.tv/' and bvid[15:] != ''):
+    for j in bvid.split('?'):
+        for i in j.split('/'):
+            if i[:2].upper() == 'BV':
+                bvid = i[:2].upper()+i[2:]
+                break
+
+if bvid[:2].upper() != 'BV':
+    logging.critical(f'"{bvid}"不是正确的BV号，请输入正确的BV号')
+    os.system('pause')
+    exit(-1)
+
+api_url = f'https://api.bilibili.com/x/web-interface/view?bvid={bvid}'
+s_url = f'https://b23.tv/{bvid[:2].upper()+bvid[2:]}'
+data_s = urllib.request.urlopen(api_url)
+data_s = json.loads(data_s.read().decode('utf-8'))
+
+if data_s['code'] != 0:
+    logging.critical(f'不存在"{bvid}"，你确定吗？')
+    os.system('pause')
+    exit(-1)
+
+print(f"\n[{data_s['data']['title']} - {data_s['data']['owner']['name']}] {s_url}\n")
+os.system('pause')
---
+++ 合并视频.py
@@ -0,0 +1,23 @@
+import os
+
+f1 = ''
+f2 = ''
+f3 = ''
+
+while True:
+    f1 = input('输入第一个文件')
+    if f1:
+        break
+
+while True:
+    f2 = input('输入第一个文件')
+    if f2:
+        break
+
+f3 = f2[:-1]+'.new.mp4"'
+
+print(f'ffmpeg -i {f1} -i {f2} -c copy {f3}')
+with open('tmp.bat', 'w') as f:
+    f.write(f'ffmpeg -i {f1} -i {f2} -c copy {f3}')
+os.system('tmp.bat')
+os.remove('tmp.bat')
