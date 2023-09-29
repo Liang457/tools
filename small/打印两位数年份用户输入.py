5953bee3c051d155a7876c44bdb702dcf8b8f683
Cool-GK <liang2457@qq.com>
2023-09-29 22:12:44 +0800

    23Y39WA 修改random-name的读我，添加更新日志 修改small\打印两位数年份用户输入.py的输出格式

--- small/打印两位数年份用户输入.py
+++ small/打印两位数年份用户输入.py
@@ -9,4 +9,6 @@ week = now.isocalendar()[1]
 # 提示用户输入一个字符串
 input_str = input("请输入一个字符串：")[:1]
 # 输出结果
-print(f"{year}W{week}{input_str}")
+print(f"{year}Y{week}W{input_str}")
+
+input('键入 Enter 退出: ')
